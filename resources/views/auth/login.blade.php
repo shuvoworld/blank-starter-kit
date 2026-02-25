16302e81c47da76126b8e016ddb330cdf8dd6209
sanjid29 <sanjidhabib@gmail.com>
2026-02-25 20:57:02 +0600

    #added auth layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.guest')
+@extends('layouts.auth.guest')
 
 @section('content')
 <!-- Session Status -->
---
+++ resources/views/layouts/auth/guest.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.2/css/all.min.css">
+</head>
+<body class="login-page bg-body-secondary">
+    <div class="login-box">
+        @hasSection('content-top')
+            @yield('content-top')
+        @else
+            {{-- Default fallback content --}}
+            <div class="login-logo">
+                <a href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</a>
+            </div>
+        @endif
+
+        @yield('content')
+    </div>
+
+    @stack('scripts')
+</body>
+</html>
