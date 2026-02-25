d201aee684aba6f254c5933d82782c29309e345d
sanjid29 <sanjidhabib@gmail.com>
2026-02-25 20:19:06 +0600

    #

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '4234382c9ff1e788e11f6a2074686d33fba03e08',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -452,9 +452,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '4234382c9ff1e788e11f6a2074686d33fba03e08',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
